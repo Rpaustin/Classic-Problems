6c251d21831cc6ee8ec27c25a5a0743e4d261b3b
Rhys Austin <rhyspaustin@gmail.com>
2017-03-02 11:39:23 +1100

    Added quantity hash structure

--- DataStructures.js
+++ DataStructures.js
@@ -17,3 +17,47 @@ exports.HashSet = class HashSet {
       return this.set[key] === this.val;
     }
 };
+
+// HashSet that keeps track of quantities of each key
+exports.QHashSet = class QHashSet {
+    constructor() {
+      this.set = [];
+      this.val = 1;
+    }
+
+    // Set key and value as val (No specific value needed)
+    add(key) {
+        this.set[key] = this.val;
+    }
+
+    // If the key is in the set, it will equal this.val as it would
+    // have been set in the "add" function
+    contains(key) {
+      return this.set[key] >= 1;
+    }
+
+    increment(key) {
+      if(this.contains(key))
+        this.set[key]++;
+    }
+
+    getCount(key) {
+      return this.set[key];
+    }
+
+    getMaxCount() {
+      if(this.set.length == 0)
+        return null;
+
+      var theSet = this.set;
+      var theKeys = Object.keys(theSet);
+      var maxCount = theSet[theKeys[0]];
+
+      for(var i = 0; i < theKeys.length; i++) {
+        if(maxCount < theSet[theKeys[i]])
+          maxCount = theSet[theKeys[i]];
+      }
+
+      return maxCount;
+    }
+};
--- Frequent Integer/main.js
+++ Frequent Integer/main.js
@@ -8,6 +8,20 @@
 var DataStructures = require("../DataStructures.js");
 
 function getMostFrequentInteger(data) {
-  
+  var integerSet = new DataStructures.QHashSet();
+
+  for(var i = 0; i < data.length; i++) {
+    if(integerSet.contains(data[i]))
+      integerSet.increment(data[i]);
+    else {
+      integerSet.add(data[i]);
+    }
+  }
+
+  integerSet.getMaxCount();
 
 }
+
+var data = [1,1,2,2,3,3,3,3,300];
+
+console.log(getMostFrequentInteger(data));
